90651e8e8dd43df380c25f669a257fcb7bf0d9dd
Roy OSSAI <royossai@yahoo.com>
2025-06-14 08:23:22 +0200

    fix: update photo backdrop URL logic for improved video playback handling

--- backend/src/misc/llm.service.ts
+++ backend/src/misc/llm.service.ts
@@ -1,5 +1,5 @@
 import { accessibleBy } from '@casl/prisma';
-import { AppAbilityType, Action } from '@eleven-am/authorizer';
+import { Action, AppAbilityType } from '@eleven-am/authorizer';
 import { TaskEither } from '@eleven-am/fp';
 import { PrismaVectorStore } from '@langchain/community/vectorstores/prisma';
 import { OpenAIEmbeddings } from '@langchain/openai';
@@ -14,7 +14,6 @@ import { OPEN_AI_API_KEY_SYMBOL } from '../config/constants';
 import { PrismaService } from '../prisma/prisma.service';
 import { ScanResult } from '../scanner/scanner.contracts';
 import { firstLetterUpperCase } from '../utils/helper.fp';
-
 
 @Injectable()
 export class LLMService {
@@ -33,23 +32,25 @@ export class LLMService {
         }
     }
 
-    static generateMediaVectorStore (db: PrismaService, openAIApiKey: string): PrismaVectorStore<MediaEmbeds, 'MediaEmbeds', any, any> {
-        return PrismaVectorStore.withModel<MediaEmbeds>(db)
-            .create(
-                new OpenAIEmbeddings({
-                    openAIApiKey,
-                }),
-                {
-                    prisma: Prisma,
-                    tableName: 'MediaEmbeds',
-                    vectorColumnName: 'vector',
-                    columns: {
-                        id: PrismaVectorStore.IdColumn,
-                        mediaId: true,
-                        metadata: PrismaVectorStore.ContentColumn,
-                    },
+    static generateMediaVectorStore (
+        db: PrismaService,
+        openAIApiKey: string,
+    ): PrismaVectorStore<MediaEmbeds, 'MediaEmbeds', any, any> {
+        return PrismaVectorStore.withModel<MediaEmbeds>(db).create(
+            new OpenAIEmbeddings({
+                openAIApiKey,
+            }),
+            {
+                prisma: Prisma,
+                tableName: 'MediaEmbeds',
+                vectorColumnName: 'vector',
+                columns: {
+                    id: PrismaVectorStore.IdColumn,
+                    mediaId: true,
+                    metadata: PrismaVectorStore.ContentColumn,
                 },
-            );
+            },
+        );
     }
 
     saveMediaEmbed (media: ScanResult, mediaId: string) {
@@ -71,110 +72,110 @@ export class LLMService {
             .map(([key, value]) => `*${firstLetterUpperCase(key)}* ${value}\n`)
             .join('');
 
-        const addDocumentsTryCatch = (
-            document: Document<{ id: string, mediaId: string, metadata: string }>,
-        ) => TaskEither.tryCatch(
-            () => this.prisma.addDocuments([document]),
-            'Error adding media embeddings to vector store',
-        );
-
-        return TaskEither
-            .tryCatch(
-                () => this.db.mediaEmbeds.create({
-                    data: {
-                        mediaId,
-                        metadata: embeddableMediaMetadata,
+        return TaskEither.tryCatch(
+            () => this.db.mediaEmbeds.create({
+                data: {
+                    mediaId,
+                    metadata: embeddableMediaMetadata,
+                },
+            }),
+            'Error saving media embeddings',
+        )
+            .map(
+                (embed) => new Document({
+                    pageContent: embed.metadata,
+                    metadata: {
+                        id: embed.id,
+                        mediaId: embed.mediaId,
+                        metadata: embed.metadata,
                     },
                 }),
-                'Error saving media embeddings',
             )
-            .map((embed) => new Document({
-                pageContent: embed.metadata,
-                metadata: {
-                    id: embed.id,
-                    mediaId: embed.mediaId,
-                    metadata: embed.metadata,
-                },
-            }))
-            .chain(addDocumentsTryCatch)
+            .fromPromise((docs) => this.prisma.addDocuments([docs]))
             .map(() => mediaId);
     }
 
-    searchForMostSimilarMedia (query: string, limit: number, ability: AppAbilityType) {
+    searchForMostSimilarMedia (
+        query: string,
+        limit: number,
+        ability: AppAbilityType,
+    ) {
         return TaskEither.tryCatch(
             () => this.prisma.similaritySearch(query, limit),
             'Error searching for most similar media',
         )
             .map((documents) => documents.map((document) => document.metadata.mediaId))
-            .chain((mediaIds) => TaskEither.tryCatch(
-                () => this.db.media.findMany({
-                    where: {
-                        AND: [
-                            {
-                                id: {
-                                    'in': mediaIds,
-                                },
+            .fromPromise((mediaIds) => this.db.media.findMany({
+                where: {
+                    AND: [
+                        {
+                            id: {
+                                'in': mediaIds,
                             },
-                            accessibleBy(ability, Action.Read).Media,
-                        ],
-                    },
-                }),
-                'Error getting media from similar media ids',
-            ))
+                        },
+                        accessibleBy(ability, Action.Read).Media,
+                    ],
+                },
+            }))
             .orElse(() => TaskEither.of([]));
     }
 
-    generateMediaRecommendations (mediaId: string, ability: AppAbilityType, amount = 20) {
-        return this.getEmbeddings(mediaId).chain(({ vector }) => TaskEither.of(vector).matchTask<Media[]>([
-            {
-                predicate: (vector) => vector.length > 0,
-                run: (vector) => TaskEither.tryCatch(
-                    () => this.prisma.similaritySearchVectorWithScore(
-                        vector,
-                        amount + 1,
-                    ),
-                    'Error getting closest embeddings',
-                )
-                    .map((data) => data.map(([vector, score]) => ({
-                        score,
-                        mediaId: vector.metadata.mediaId,
-                    })))
-                    .chain((mappedData) => TaskEither.tryCatch(
-                        () => this.db.media.findMany({
-                            where: {
-                                AND: [
-                                    {
-                                        id: {
-                                            'in': mappedData
-                                                .filter((data) => data.mediaId !== mediaId)
-                                                .map((data) => data.mediaId),
+    generateMediaRecommendations (
+        mediaId: string,
+        ability: AppAbilityType,
+        amount = 20,
+    ) {
+        return this.getEmbeddings(mediaId).chain(({ vector }) => TaskEither.of(vector)
+            .matchTask<Media[]>([
+                {
+                    predicate: (vector) => vector.length > 0,
+                    run: (vector) => TaskEither.tryCatch(
+                        () => this.prisma.similaritySearchVectorWithScore(
+                            vector,
+                            amount + 1,
+                        ),
+                        'Error getting closest embeddings',
+                    )
+                        .map((data) => data.map(([vector, score]) => ({
+                            score,
+                            mediaId: vector.metadata.mediaId,
+                        })))
+                        .chain((mappedData) => TaskEither.tryCatch(
+                            () => this.db.media.findMany({
+                                where: {
+                                    AND: [
+                                        {
+                                            id: {
+                                                'in': mappedData
+                                                    .filter((data) => data.mediaId !== mediaId)
+                                                    .map((data) => data.mediaId),
+                                            },
                                         },
-                                    },
-                                    accessibleBy(ability, Action.Read).Media,
-                                ],
-                            },
-                        }),
-                        'Error getting media recommendations',
-                    ).intersect(mappedData, 'id', 'mediaId', ['score']))
-                    .sortBy('score', 'asc'),
-            },
-            {
-                predicate: () => true,
-                run: () => TaskEither.tryCatch(() => this.db.media.random({
-                    length: amount,
-                    where: {
-                        AND: [
-                            {
-                                id: {
-                                    not: mediaId,
+                                        accessibleBy(ability, Action.Read).Media,
+                                    ],
                                 },
-                            },
-                            accessibleBy(ability, Action.Read).Media,
-                        ],
-                    },
-                })),
-            },
-        ])
+                            }),
+                            'Error getting media recommendations',
+                        ).intersect(mappedData, 'id', 'mediaId', ['score']))
+                        .sortBy('score', 'asc'),
+                },
+                {
+                    predicate: () => true,
+                    run: () => TaskEither.tryCatch(() => this.db.media.random({
+                        length: amount,
+                        where: {
+                            AND: [
+                                {
+                                    id: {
+                                        not: mediaId,
+                                    },
+                                },
+                                accessibleBy(ability, Action.Read).Media,
+                            ],
+                        },
+                    })),
+                },
+            ])
             .map((media) => ({
                 recommended: vector.length > 0,
                 media,
@@ -184,7 +185,10 @@ export class LLMService {
     @OnEvent(OPEN_AI_KEY_UPDATED_EVENT)
     openAIKeyUpdated (event: OpenAIKeyEvent) {
         try {
-            this.prisma = LLMService.generateMediaVectorStore(this.db, event.openAIKey);
+            this.prisma = LLMService.generateMediaVectorStore(
+                this.db,
+                event.openAIKey,
+            );
         } catch (error) {
             this.logger.error('Error updating openAI key', error);
         }
@@ -198,18 +202,16 @@ export class LLMService {
             LIMIT 1
         `;
 
-        return TaskEither
-            .tryCatch(
-                () => this.db.$queryRaw<MediaEmbeddable[]>(query),
-                'Error getting media embeddings',
-            )
+        return TaskEither.tryCatch(
+            () => this.db.$queryRaw<MediaEmbeddable[]>(query),
+            'Error getting media embeddings',
+        )
             .map((items) => items[0])
             .nonNullable('No embeddings found for media id')
-            .chain((embed) => TaskEither
-                .tryCatch(
-                    () => this.db.media.findUnique({ where: { id: embed.mediaId } }),
-                    'Error getting media embeddings',
-                )
+            .chain((embed) => TaskEither.tryCatch(
+                () => this.db.media.findUnique({ where: { id: embed.mediaId } }),
+                'Error getting media embeddings',
+            )
                 .nonNullable('No media found for media id')
                 .map((media) => ({
                     ...embed,
